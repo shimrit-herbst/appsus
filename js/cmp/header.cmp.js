3379c5dd7f75365088e4d6707e9724149bcb3f25
Shimrit Herbst <shimrit.levine@gmail.com>
2020-11-06 23:01:00 +0200

    Home page design

--- js/cmp/header.cmp.js
+++ js/cmp/header.cmp.js
@@ -2,14 +2,27 @@ export default {
     name: 'appsus-app-header',
     template: `
     <section class="appsus-app-header">
-        <nav>
-            <router-link to="/" >Home</router-link>|
-            <router-link to="/email" >Mister Email</router-link>|
-            <router-link to="/keep" >Miss Keep</router-link>|
-            <!-- <router-link to="/book" >Miss Books</router-link>| -->
-            <router-link to="/about" >About Us</router-link>|
-            <img src="http://luminouspil.com/app/themes/luminouspil/dist/images/luminous_logo.png" />          
+        <nav class="main-nav">
+            <div class="nav-img">
+                <img src="http://luminouspil.com/app/themes/luminouspil/dist/images/luminous_logo.png" />          
+            </div>
+            <div class="screen" @click="toggleMenu"></div>
+            <button class="hamburger-sign">☰</button>
+            <ul class="main-nav-list" @click="toggleMenu"> 
+                <!-- main-nav should be hidden, add toggle function when clicking button  -->
+                <li><router-link to="/" >Home</router-link></li>
+                <li><router-link to="/email" >Mister Email</router-link></li>
+                <li><router-link to="/keep" >Miss Keep</router-link></li>
+                <li><router-link to="/book" >Miss Books</router-link></li>
+                <li><router-link to="/about" >About Us</router-link></li>
+            </ul>
         </nav>
     </section>
     `,
+    methods: {
+        toggleMenu() {
+            document.body.classList.toggle('menu-open');
+        }
+    }
 }
+
